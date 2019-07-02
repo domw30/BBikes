86589690e90d5693ab551590228beb7115ad2858
dominic white <domw30@gmail.com>
2019-07-02 11:16:09 +0100

    add bike respond_to working test, add return to working method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,14 @@
 require 'docking_station'
 
 describe DockingStation do
-    it "should respond to #release_bike" do
-      docking_station = DockingStation.new
-      expect(docking_station).to respond_to :release_bike
-    end
+  it "should respond to #release_bike" do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to :release_bike
+  end
+
+  it "releases working bikes" do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).to be_working
+  end
 end
