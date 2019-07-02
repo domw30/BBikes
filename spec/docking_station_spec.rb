f5b66692d338e60cf110929c825eadac2f480825
dominic white <domw30@gmail.com>
2019-07-02 11:41:08 +0100

    add dock test and return to dock test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,29 @@ describe DockingStation do
     bike = docking_station.release_bike
     expect(bike).to be_working
   end
+
+  it "should respond to #dock with 1 argument" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    expect(docking_station).to respond_to(:dock).with(1).argument
+  end
+
+  it "should respond to #bike" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    expect(docking_station).to respond_to(:bike)
+  end
+
+  it "should dock something" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    expect(docking_station.dock(bike)).to eq bike
+  end
+
+  it "should return docked bikes" do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect(docking_station.bike).to eq bike
+  end
 end
