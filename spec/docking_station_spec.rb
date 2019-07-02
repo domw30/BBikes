fcd7e143383de8220452c9d36493987a1cdcd65e
dominic white <domw30@gmail.com>
2019-07-02 13:00:03 +0100

    add raise exception when docking station full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -35,15 +35,21 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
-   it 'releases a bike' do
-     bike = Bike.new
-     docking_station.dock(bike)
-     expect(docking_station.release_bike).to eq bike
-   end
+    it 'releases a bike' do
+      bike = Bike.new
+      docking_station.dock(bike)
+      expect(docking_station.release_bike).to eq bike
+    end
 
-   it 'raises an error when there are no bikes available' do
+    it 'raises an error when there are no bikes available' do
      expect { subject.release_bike }.to raise_error 'No bikes available'
-   end
- end
- 
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
 end
