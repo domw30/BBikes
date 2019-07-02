3c0ce56905be452baadcf87249065decca65fcee
dominic white <domw30@gmail.com>
2019-07-02 13:20:12 +0100

    refactor dockingstation tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,42 +3,33 @@ require 'docking_station'
 describe DockingStation do
 
   subject(:docking_station) { described_class.new }
+  subject(:bike) { described_class.new }
 
   it "should respond to #release_bike" do
-    docking_station = DockingStation.new
-    expect(docking_station).to respond_to :release_bike
+    expect(subject).to respond_to :release_bike
   end
 
   it "should respond to #dock with 1 argument" do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    expect(docking_station).to respond_to(:dock).with(1).argument
+    expect(subject).to respond_to(:dock).with(1).argument
   end
 
   it "should respond to #bike" do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    expect(docking_station).to respond_to(:bike)
+    expect(subject).to respond_to(:bike)
   end
 
   it "should dock something" do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    expect(docking_station.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq bike
   end
 
   it "should return docked bikes" do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    docking_station.dock(bike)
-    expect(docking_station.bike).to eq bike
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 
   describe '#release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
-      docking_station.dock(bike)
-      expect(docking_station.release_bike).to eq bike
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
     end
 
     it 'raises an error when there are no bikes available' do
